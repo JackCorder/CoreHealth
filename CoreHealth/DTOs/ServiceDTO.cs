505f7a9d3d06b55ce5e62040e1afa6ed83f49dfe
JackCorder <79921206+JackCorder@users.noreply.github.com>
2025-06-14 12:16:32 -0600

    ClinicHistoryDTO, PrescriptionMedicationDTO, ServiceDTO Created

---
+++ CoreHealth/DTOs/ServiceDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreHealth.DTOs
+{
+    public class ServiceDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Servicio")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Descripcion")]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "Precio")]
+        public decimal Cost { get; set; }
+    }
+}
