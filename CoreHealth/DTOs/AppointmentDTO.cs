b6365583eeb9ac256899b746bb90d27855722f6e
Zhylian-bop <zhylian21@gmail.com>
2025-06-14 12:52:50 -0600

    Properties, error messages, and names in view added for DTOs of Doctor, Appointment, and Clinic.

--- CoreHealth/DTOs/AppointmentDTO.cs
+++ CoreHealth/DTOs/AppointmentDTO.cs
@@ -1,6 +1,27 @@
-﻿namespace CoreHealth.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreHealth.DTOs
 {
     public class AppointmentDTO
     {
+        public int Id { get; set; }
+        [Display(Name ="Fecha de la cita")]
+        [Required(ErrorMessage ="Se debe seleccionar una fecha")]
+        public DateTime Date { get; set; }
+        [Display(Name ="Paciente")]
+        [Required(ErrorMessage ="Debe asignarse un paciente")]
+        public int PatientId { get; set; }
+        [Display(Name ="Consultorio")]
+        [Required(ErrorMessage ="Debe seleccionarse un consultorio para la cita")]
+        public int ClinicId { get; set; }
+        [Display(Name ="Motivo de la cita")]
+        [Required(ErrorMessage ="Debe espeficiar el motivo de la cita")]
+        public string Reason { get; set; }
+        [Display(Name ="Diagnostico Medico")]
+        public string? Diagnostic { get; set; }
+        [Display(Name ="Tratamiento recomendado")]
+        public string? Treatment { get; set; }
+        [Display(Name ="Servicio ofrecido")]
+        public int? ServiceId { get; set; }
     }
 }
--- CoreHealth/DTOs/ClinicDTO.cs
+++ CoreHealth/DTOs/ClinicDTO.cs
@@ -1,6 +1,17 @@
-﻿namespace CoreHealth.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreHealth.DTOs
 {
     public class ClinicDTO
     {
+        public int Id { get; set; }
+        [Display(Name="Consultorio")]
+        [Required(ErrorMessage ="Se requiere un nombre del consultorio")]
+        public string Name { get; set; }
+        [Display(Name = "Descripcion del consultorio")]
+        public string? Description { get; set; }
+        [Display(Name ="Doctor Asignado")]
+        public int? DoctorId { get; set; }
+
     }
 }
