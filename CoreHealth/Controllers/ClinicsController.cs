6820e43132a4fa7ab062571d3a08abb65e815aa6
Zhylian-bop <zhylian21@gmail.com>
2025-06-15 21:31:20 -0600

    Implemented get all, get by id, create, update and delete endpoints for Appointment Service, Clinic Service and  Doctor Service.

---
+++ CoreHealth/Controllers/ClinicsController.cs
@@ -0,0 +1,91 @@
+﻿using CoreHealth.DTOs;
+using CoreHealth.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreHealth.Controllers
+{
+    [Route("api/v1/clinic")]
+    [ApiController]
+    public class ClinicsController : ControllerBase
+    {
+        private readonly IClinicService _clinicService;
+        public ClinicsController(IClinicService clinicService) { 
+            _clinicService = clinicService;
+        }
+        // GET: api/<clinicController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var clinics = await _clinicService.GetAllAsync();
+            return Ok(clinics);
+        }
+
+        // GET api/<clinicController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var clinic = await _clinicService.GetByIdAsync(id);
+
+            if (clinic == null)
+            {
+                return NotFound(new { message = "La marca no existe" });     // Respuesta HTTP 404 Not Found con un mensaje
+            }
+
+            return Ok(clinic);                                                 // Retorna 200 OK con el producto encontrado.
+        }
+
+        // POST api/<clinicController>
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Create([FromForm] ClinicDTO clinicDTO)
+        {
+            try
+            {
+                await _clinicService.AddAsync(clinicDTO);
+                return CreatedAtAction(nameof(GetById), new { id = clinicDTO.Id }, clinicDTO);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Hubo un error al crear la marca: {ex.Message}" });
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromForm] ClinicDTO clinicDTO)
+        {
+            if (id != clinicDTO.Id)
+                return BadRequest(new { message = "El ID proporcionado no coincide con el objeto" });
+
+            try
+            {
+                await _clinicService.UpdateAsync(clinicDTO);
+                return NoContent(); // 204: Indica éxito sin devolver contenido adicional
+            }
+            catch (ApplicationException ex)
+            {
+                return NotFound(new { message = ex.Message }); // Manejo de error si la marca no existe
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Error al actualizar la marca: {ex.Message}" });
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _clinicService.DeleteAsync(id);
+                return NoContent(); // 204: Confirma la eliminación sin devolver datos
+            }
+            catch (ApplicationException ex)
+            {
+                return NotFound(new { message = ex.Message }); // Si la marca no existe
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Error al eliminar la marca: {ex.Message}" });
+            }
+        }
+    }
+}
---
+++ CoreHealth/Controllers/DoctorsController.cs
@@ -0,0 +1,92 @@
+﻿using CoreHealth.DTOs;
+using CoreHealth.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreHealth.Controllers
+{
+    [Route("api/v1/doctors")]
+    [ApiController]
+    public class DoctorsController : ControllerBase
+    {
+        private readonly IDoctorService _doctorService;
+        public DoctorsController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+        // GET: api/<clinicController>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var clinics = await _doctorService.GetAllAsync();
+            return Ok(clinics);
+        }
+
+        // GET api/<clinicController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var clinic = await _doctorService.GetByIdAsync(id);
+
+            if (clinic == null)
+            {
+                return NotFound(new { message = "La marca no existe" });     // Respuesta HTTP 404 Not Found con un mensaje
+            }
+
+            return Ok(clinic);                                                 // Retorna 200 OK con el producto encontrado.
+        }
+
+        // POST api/<clinicController>
+        [HttpPost]
+        [Consumes("multipart/form-data")]
+        public async Task<IActionResult> Create([FromForm] DoctorDTO doctorDTO)
+        {
+            try
+            {
+                await _doctorService.AddAsync(doctorDTO);
+                return CreatedAtAction(nameof(GetById), new { id = doctorDTO.Id }, doctorDTO);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Hubo un error al crear la marca: {ex.Message}" });
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromForm] DoctorDTO doctorDTO)
+        {
+            if (id != doctorDTO.Id)
+                return BadRequest(new { message = "El ID proporcionado no coincide con el objeto" });
+
+            try
+            {
+                await _doctorService.UpdateAsync(doctorDTO);
+                return NoContent(); // 204: Indica éxito sin devolver contenido adicional
+            }
+            catch (ApplicationException ex)
+            {
+                return NotFound(new { message = ex.Message }); // Manejo de error si la marca no existe
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Error al actualizar la marca: {ex.Message}" });
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _doctorService.DeleteAsync(id);
+                return NoContent(); // 204: Confirma la eliminación sin devolver datos
+            }
+            catch (ApplicationException ex)
+            {
+                return NotFound(new { message = ex.Message }); // Si la marca no existe
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = $"Error al eliminar la marca: {ex.Message}" });
+            }
+        }
+    }
+}
